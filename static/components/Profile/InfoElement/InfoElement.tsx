0879ec514bde63771ed66a8f24fbf633d5b26481
nikita <nik.pashmentov@ya.ru>
2020-05-13 22:23:14 +0300

    Refactoring code

--- static/components/Profile/InfoElement/InfoElement.tsx
+++ static/components/Profile/InfoElement/InfoElement.tsx
@@ -13,11 +13,11 @@ const TITLE_NO_INFO = 'Edit to set your ';
 
 export function InfoElement(props: InfoElementProps): React.ReactElement {
     const { name, value, title } = props;
-    const fullTitle = value ? '' : TITLE_NO_INFO + title;
+    const fullTitle = !value && TITLE_NO_INFO + title || '';
     return (
         <div className={css.root}>
             <div className={css.name}>{`${name}:`}</div>
-            <div className={css.value} title={fullTitle}>{value ? value : NO_INFO}</div>
+            <div className={css.value} title={fullTitle}>{value || NO_INFO}</div>
         </div>
     );
 }
